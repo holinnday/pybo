867b4aee0a5456d5ca10b60391301ce3b5334079
holinnday <innlee0326@gmail.com>
2023-06-20 18:03:09 +0900

    pybo#4-2

--- pybo/urls.py
+++ pybo/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('comment/delete/answer/<int:comment_id>/', comment_views.comment_delete_answer, name='comment_delete_answer'),
 
     #vote_views.py
-    path('vote/create/<int:question_id>/', vote_views.vote_question, name='vote_question'),
+    path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
+    path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
 ]
